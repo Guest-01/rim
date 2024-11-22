bcce3fb4d5b74c7c67609ea49f577ae8dd41825c
atWork <cloudonly21@gmail.com>
2024-11-22 16:06:02 +0900

    hide admin menu when not admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
+import { getSession } from "./lib/auth";
+import prisma from "./lib/prisma";
 
 const noto = Noto_Sans_KR({ subsets: ["latin"] });
 
@@ -12,14 +14,17 @@ export const metadata: Metadata = {
   description: "improved alternative of redmine",
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+  const session = await getSession();
+  const user = await prisma.account.findFirst({ where: { id: session?.accountId } });
+  
   return (
     <html lang="en">
       <body className={noto.className}>
         <NavBar />
         {/* 전체 화면에서 헤더(4rem)과 헤더의 아랫 보더(1px)를 뺀 값이 높이 */}
         <div className="flex" style={{ height: "calc(100vh - 4rem - 1px)" }}>
-          <SideBar />
+          <SideBar isAdmin={user?.roleId === 1} />
           <main className="p-4 pt-0 w-full">
             {children}
           </main>
