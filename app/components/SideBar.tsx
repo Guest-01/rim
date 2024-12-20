5f1e861af7595255ad12743e4e3d0d3be7c01ea6
atWork <cloudonly21@gmail.com>
2024-12-20 16:20:31 +0900

    remove hidden class in sidebar, add credentials on login default for demo

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -12,7 +12,7 @@ export default function SideBar({ isAdmin }: { isAdmin: boolean }) {
   }
 
   return (
-    <ul className="menu p-4 min-w-64 bg-base-200 text-base-content hidden lg:block space-y-1">
+    <ul className="menu p-4 min-w-64 bg-base-200 text-base-content space-y-1">
       <Link href="/issues/new" className="btn btn-sm btn-outline w-full">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
           <path fillRule="evenodd" d="M4 2a1.5 1.5 0 0 0-1.5 1.5v9A1.5 1.5 0 0 0 4 14h8a1.5 1.5 0 0 0 1.5-1.5V6.621a1.5 1.5 0 0 0-.44-1.06L9.94 2.439A1.5 1.5 0 0 0 8.878 2H4Zm4.75 4.75a.75.75 0 0 0-1.5 0v1.5h-1.5a.75.75 0 0 0 0 1.5h1.5v1.5a.75.75 0 0 0 1.5 0v-1.5h1.5a.75.75 0 0 0 0-1.5h-1.5v-1.5Z" clipRule="evenodd" />
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,10 +12,10 @@ export default function Login() {
       <div className="card-body">
         <form action={formAction} className="form-control">
           <label htmlFor="email" className="label label-text">이메일</label>
-          <input type="email" name="email" placeholder="id@domain.com" className="input input-sm input-bordered" required />
+          <input type="email" name="email" placeholder="id@domain.com" className="input input-sm input-bordered" required value={"test1@test.com"} />
           <label htmlFor="password" className="label label-text">비밀번호</label>
           <input type="password" pattern="(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*,.?;~]{6,16}"
-            title="6~16자리의 알파벳과 숫자 조합이 필요합니다" name="password" placeholder="******" className="input input-sm input-bordered" required />
+            title="6~16자리의 알파벳과 숫자 조합이 필요합니다" name="password" placeholder="******" className="input input-sm input-bordered" required value={"admin123!"} />
           <div className="my-2 text-center text-error">{errMsg}</div>
           <SubmitBtn />
         </form>
